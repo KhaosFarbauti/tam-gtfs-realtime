bec01e63c76fca206b443772e9b0cf2c9ca9d134
Khaos Farbauti Ibn Oblivion <propserv@hotmail.com>
2024-11-07 15:56:25 +0100

    mise en forme css

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ $alerts = getServiceAlerts();
 <head>
     <meta charset="UTF-8">
     <title>Retards et alertes du réseau TaM</title>
+	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
 	<h1>Informations du réseau TaM</h1>
@@ -29,42 +30,46 @@ $alerts = getServiceAlerts();
     <p><strong>Nombre de bus/tram en retard : </strong><?= $delaysCount ?><br /><strong>Nombre de bus/tram à l'heure : </strong><?= $ontimeCount ?><br /><strong>Nombre de bus/tram en avance : </strong><?= $earlyCount ?></p>
 
 	<h2>Incidents en cours</h2>
-    <?php if (!empty($alerts)): ?>
-        <ul>
-            <?php foreach ($alerts as $alert): ?>
-                <li>
-                    <strong>Gravité : </strong><?= htmlspecialchars($alert['severity']) ?><br>
-                    <strong>Titre : </strong><?= htmlspecialchars($alert['header']) ?><br>
-                    <strong>Description : </strong><?= htmlspecialchars($alert['description']) ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php else: ?>
-        <p>Aucun incident en cours pour le moment.</p>
-    <?php endif; ?>
+<?php if (!empty($alerts)): ?>
+	<ul>
+<?php foreach ($alerts as $alert): ?>
+		<li>
+			<strong>Gravité : </strong><?= htmlspecialchars($alert['severity']) ?><br>
+			<strong>Titre : </strong><?= htmlspecialchars($alert['header']) ?><br>
+			<strong>Description : </strong><?= htmlspecialchars($alert['description']) ?>
+		</li>
+<?php endforeach; ?>
+	</ul>
+<?php else: ?>
+	<p>Aucun incident en cours pour le moment.</p>
+<?php endif; ?>
 
-    <h2>Liste des retards du réseau TaM</h2>
-    <?php if (!empty($delays)): ?>
-        <table>
-            <tr>
-                <th>Route</th>
-                <th>Arrêt</th>
-                <th>Retard</th>
+	<h2>Liste des retards du réseau TaM</h2>
+<?php if (!empty($delays)): ?>
+	<table class="dataTable">
+		<thead>
+			<tr>
+				<th>Route</th>
+				<th>Arrêt</th>
+				<th>Retard</th>
 				<th>Heure de départ estimée</th>
-            </tr>
-            <?php foreach ($delays as $delay): ?>
-				<?php if ($delay['delay']!=0): ?>
-                <tr>
-                    <td style="text-align: center; vertical-align: middle;"><?= htmlspecialchars($delay['route']) ?></td>
-                    <td style="text-align: center; vertical-align: middle;"><?= htmlspecialchars($delay['stop']) ?></td>
-                    <td style="text-align: center; vertical-align: middle;"><?= htmlspecialchars($delay['delay']) ?></td>
-					<td style="text-align: center; vertical-align: middle;"><?= htmlspecialchars($delay['estimated_departure']) ?></td>
-                </tr>
-				<?php endif; ?>
-            <?php endforeach; ?>
-        </table>
-    <?php else: ?>
-        <p>Aucun retard détecté pour le moment.</p>
-    <?php endif; ?>
+			</tr>
+		</thead>
+		<tbody>
+<?php foreach ($delays as $delay): ?>
+<?php if ($delay['delay']!=0): ?>
+			<tr>
+				<td><?= htmlspecialchars($delay['route']) ?></td>
+				<td><?= htmlspecialchars($delay['stop']) ?></td>
+				<td><?= htmlspecialchars($delay['delay']) ?></td>
+				<td><?= htmlspecialchars($delay['estimated_departure']) ?></td>
+			</tr>
+<?php endif; ?>
+<?php endforeach; ?>
+		</tbody>
+	</table>
+<?php else: ?>
+	<p>Aucun retard détecté pour le moment.</p>
+<?php endif; ?>
 </body>
 </html>
