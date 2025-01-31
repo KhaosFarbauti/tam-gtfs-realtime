17efc1dea77b3aa4cf7bb3ec71144896ca9e0459
Khaos Farbauti Ibn Oblivion <propserv@hotmail.com>
2025-01-31 09:54:08 +0100

    ajout de liens pour les graphs

--- graphiques.php
+++ graphiques.php
@@ -28,10 +28,10 @@ include 'gtfs.php';
 <body>
     <h1>Etat global</h1>
     <div class="graph">
-        <img src="/graphs/pourcentage_retard.php" alt="Pourcentage de lignes en retard">
+        <a href="/graphs/pourcentage_retard.php"><img src="/graphs/pourcentage_retard.php" alt="Pourcentage de lignes en retard"></a>
     </div>
 	<div class="graph">
-        <img src="/graphs/moyenne_globale.php" alt="Retard moyen">
+        <a href="/graphs/moyenne_globale.php"><img src="/graphs/moyenne_globale.php" alt="Retard moyen"></a>
     </div>
 	<p><a href="/">Voir les donn&eacute;es temps r&eacute;el</a></p>
 	<h1>Retard par lignes</h1>
@@ -43,7 +43,7 @@ include 'gtfs.php';
 ?>
 	<h2><?= htmlspecialchars($routeName) ?></h2>
 	<div class="graph">
-		<img src="/graphs/retard.php?route_id=<?= htmlspecialchars($liste_lignes[$i]) ?>" alt="Retard ligne <?= htmlspecialchars($liste_lignes[$i]) ?>">
+		<a href="/graphs/retard.php?route_id=<?= htmlspecialchars($liste_lignes[$i]) ?>"><img src="/graphs/retard.php?route_id=<?= htmlspecialchars($liste_lignes[$i]) ?>" alt="Retard ligne <?= htmlspecialchars($liste_lignes[$i]) ?>"></a>
 	</div>
 <?php
 	};
