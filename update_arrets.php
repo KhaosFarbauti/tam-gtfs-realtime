c44a40a543700083eb384c2d6450241c7f958636
Khaos Farbauti Ibn Oblivion <propserv@hotmail.com>
2025-04-17 11:16:02 +0200

    nettoyage

--- update_arrets.php
+++ update_arrets.php
@@ -1,29 +1,22 @@
 <?php
 
-// URL du fichier ZIP GTFS
-$url = 'https://data.montpellier3m.fr/TAM_MMM_GTFSRT/GTFS.zip';
-// Nom local du fichier ZIP
+$url = 'https://data.montpellier3m.fr/TAM_MMM_GTFSRT/GTFS.zip'; // URL du fichier GTFS TaM
 $zipFile = 'GTFS.zip';
-// Dossier où extraire les fichiers
 $extractDir = __DIR__ . '/gtfs_extract';
 
-// Téléchargement du fichier ZIP
 file_put_contents($zipFile, fopen($url, 'r'));
 
-// Vérification de la création du fichier
 if (!file_exists($zipFile)) {
     die("Erreur : Le fichier ZIP n'a pas pu être téléchargé.\n");
 }
 
-// Création du dossier d'extraction s'il n'existe pas
 if (!is_dir($extractDir)) {
     mkdir($extractDir, 0755, true);
 }
 
-// Ouverture de l'archive ZIP
 $zip = new ZipArchive();
 if ($zip->open($zipFile) === TRUE) {
-    // Liste des fichiers à extraire
+
     $fichiersASauver = ['routes.txt', 'stops.txt'];
     
     foreach ($fichiersASauver as $fichier) {
@@ -41,7 +34,6 @@ if ($zip->open($zipFile) === TRUE) {
     die("Erreur : Impossible d'ouvrir l'archive ZIP.\n");
 }
 
-// Suppression du fichier ZIP
 unlink($zipFile);
 echo "Fichier ZIP supprimé : $zipFile\n";
 
